bd42c8ada365237054e47f71ecf7d6e347e6c9f9
Lpevan1989 <sal.bozhadze@gmail.com>
2023-09-07 18:44:57 +0400

    course

---
+++ cypress/integration/examples/Test.js
@@ -0,0 +1,16 @@
+describe('My First test', function()
+{
+it('My first Test Case', function()
+{
+cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+cy.get('.search-keyword').type('ca')
+}
+
+)
+
+}
+
+
+
+
+)
